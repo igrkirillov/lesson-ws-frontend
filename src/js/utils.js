9ba028a13d055b165c86ee5569d756841a6e433f
Igor.Kirillov <Igor.Kirillov@x5.ru>
2024-05-09 15:39:29 +0300

    frontend

--- src/js/utils.js
+++
@@ -1,9 +0,0 @@
-export function toRusFormatDate(date) {
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
-  const hours = String(date.getHours()).padStart(2, "0");
-  const minutes = String(date.getMinutes()).padStart(2, "0");
-  const sec = String(date.getSeconds()).padStart(2, "0");
-  return `${day}.${month}.${year} ${hours}:${minutes}:${sec}`;
-}
